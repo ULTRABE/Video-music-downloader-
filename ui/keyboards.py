6ac7284fe929562d370394455e9dda64ee3f6993
ULTRABE <musklogo2@gmail.com>
2026-01-21 16:54:38 +0530

    Delete ui directory

--- ui/keyboards.py
+++
@@ -1,6 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def private_button(bot_username: str):
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("🔒 Open Private Chat", url=f"https://t.me/{bot_username}")]
-    ])
