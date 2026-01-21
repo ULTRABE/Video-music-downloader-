dbdb780f37ca99680c81bf9b6054fde4c24a858b
ULTRABE <musklogo2@gmail.com>
2026-01-21 16:55:32 +0530

    Create text.py

---
+++ ui/text.py
@@ -0,0 +1,8 @@
+START = "🎥 Send a video link."
+
+ADULT_PM = (
+    "🔞 **Private Download Mode**\n\n"
+    "Send the adult video link.\n"
+    "⚠️ Video will be deleted in 1 minute.\n"
+    "Save it to Saved Messages."
+)
